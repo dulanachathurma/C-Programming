522ca882baed50b3304afe7e94260a73989a0174
DULANA CHATHURMA <dulanachathurma99@gmail.com>
2025-09-04 12:34:35 +0530

    Add files via upload

---
+++ Assignment 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+printf("\tStudent Details Form.\n");
+printf("------------------------------\n\n");
+printf("Name\tAge\t  City");
+printf("\n----------------------\n");
+printf("Sam \t22\t  Colombo\n");
+printf("John   \t20\t  Kandy\n");
+printf("Max \t25\t  Galle");
+printf("\n\nMore details: c:\\new folder\\Students");
+printf("\n\nDeveloped by\"SITEC TEAM\"Matara");
+
+getchar();
+}
